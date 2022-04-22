8a01b92cb846e7719b4437ef9170ef3d85c874d2
“Monles” <iceueb@gmail.com>
2022-04-22 13:58:10 +0800

    many screens

---
+++ src/screens/Basket/index.js
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+import restaurants from "../../../assets/data/restaurants.json";
+const restaurant = restaurants[0];
+
+const BasketDishItem = ({ basketDish }) => {
+  return (
+    <View style={styles.row}>
+      <View style={styles.quantityContainer}>
+        <Text>1</Text>
+      </View>
+      <Text style={{ fontWeight: "600" }}>{basketDish.name}</Text>
+      <Text style={{ marginLeft: "auto" }}>${basketDish.price}</Text>
+    </View>
+  );
+};
+
+const Basket = () => {
+  return (
+    <View style={styles.page}>
+      <Text style={styles.name}>{restaurant.name}</Text>
+
+      <Text>Your items</Text>
+
+      <FlatList
+        data={restaurant.dishes}
+        renderItem={({ item }) => <BasketDishItem basketDish={item} />}
+      />
+
+      <View style={styles.separator} />
+
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>Create order</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    width: "100%",
+    paddingVertical: 40,
+    padding: 10,
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  separator: {
+    height: 1,
+    backgroundColor: "lightgrey",
+    marginVertical: 10,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 15,
+  },
+  quantity: {
+    fontSize: 25,
+    marginHorizontal: 20,
+  },
+  button: {
+    backgroundColor: "black",
+    marginTop: "auto",
+    padding: 20,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 18,
+  },
+  quantity: {
+    backgroundColor: "lightgray",
+    paddingHorizontal: 5,
+    paddingVertical: 2,
+    marginRight: 5,
+    borderRadius: 3,
+  },
+});
+
+export default Basket;
